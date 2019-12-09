1f5a1d8bb39d52823f26b60d2875e6ffa4c541dd
Gonzalo Linares N <gonzalolinares@seven4n.com>
2019-12-08 19:31:06 -0500

    Added tableau content and some description according to Francisco example.

---
+++ pages/rciu.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+		<br>
+		<h1 align="center">¿Ha aumentado los bebés prematuros con RCIU en la última década?</h1>
+		<br>
+		La restricción de crecimiento ultrauterino es detectado dentro del vientre de la madre cuando el bebé no tiene el peso suficiente para la edad gestacional en la que se encuentra. Estos nacen de manera prematura antes de la semana 38 de gestación, comúnmnete con un peso menor a 2550 gr. La fundación Canguro tiene la maravillosa función de prestar ayuda a aquellas familias en que el bebé nace con esta restricción y almacena la  
+
+		La siguiente visualización, representa
+
+		<br>
+		<br>
+		
+		<div>
+			<div>
+
+			<iframe src="https://public.tableau.com/views/Flowers_15723185475430/Cantidad?:increment_view_count=no&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&publish=yes&:origin=viz_share_link&:showVizHome=no" width="1004" height="600" frameborder="0"></iframe>
+
+		</div>
+
+		</div>
+	</div>
+</template>
