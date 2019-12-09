b58f5b27a05456acfa65a1c42bab10cdb6bf8778
Gonzalo Linares N <g.linaresn@uniandes.edu.co>
2019-12-08 23:53:31 -0500

    Finished body and texts. Need to include true tableau links.

--- pages/rciu.vue
+++ pages/rciu.vue
@@ -3,9 +3,21 @@
 		<br>
 		<h1 align="center">¿Ha aumentado los bebés prematuros con RCIU en la última década?</h1>
 		<br>
-		La restricción de crecimiento ultrauterino es detectado dentro del vientre de la madre cuando el bebé no tiene el peso suficiente para la edad gestacional en la que se encuentra. Estos nacen de manera prematura antes de la semana 38 de gestación, comúnmnete con un peso menor a 2550 gr. La fundación Canguro tiene la maravillosa función de prestar ayuda a aquellas familias en que el bebé nace con esta restricción y almacena la  
+		La restricción de crecimiento ultra uterino (RCIU) es detectada, por ejemplo, dentro del vientre de la madre cuando el bebe no tiene el peso suficiente para la edad gestacional en la que se encuentra. Este nace de manera prematura antes de la semana 38 de gestación, comúnmente con un peso menor a 2500 gr. La fundación Canguro tiene la maravillosa labor de prestar ayuda a aquellas familias en la que el bebé nace con esta restricción y almacena algunos datos relevantes del recién nacido. 
 
-		La siguiente visualización, representa
+		<br>
+		<br>
+
+		En Colombia, el retraso de crecimiento ha sido prevenido por medio de consejos a las madres embarazadas, donde fumar o consumir drogas pueden significar un peligro inminente al feto en desarrollo. Pero ¿en verdad ha disminuido el RCIU de bebés que entran en el programa de la fundación Canguro? 
+
+		<br>
+
+		A continuación, se expone una visualización de la última década de la fundación canguro y los bebés que han tenido esta restricción.
+
+		<br>
+		<br>
+
+		La visualización son BoxPlot por año, donde el valor de la mitad de la caja corresponde a la mediana, es decir al 50 % de los datos y en el que indica si hay una mayor o menor cantidad de RCIU por año. Asimismo, se puede ver el detalle en cada caja y un filtro en el lado derecho con el propósito de tener más detalle del año en selección:
 
 		<br>
 		<br>
@@ -15,8 +27,64 @@
 
 			<iframe src="https://public.tableau.com/views/Flowers_15723185475430/Cantidad?:increment_view_count=no&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&publish=yes&:origin=viz_share_link&:showVizHome=no" width="1004" height="600" frameborder="0"></iframe>
 
-		</div>
+			</div>
+
+		<br>
+		<br>
+
+		De esta visualización, sin contar los meses, la cantidad de RCIU ha disminuido (como esperaba el insight), pero en los últimos años hay un aumento dentro de la mediana de los boxplot (2014 a 2017). Esto significa, independiente los bebés que hayan ingresado, que hay un aumento dentro de este periodo, en que puede ser justificado por la ley del ministerio en incluir a recién nacidos con el peso menor a 2500 gr.
+
+		<br>
+		<br> 
 
 		</div>
+
+		<h3 align="left">RCIU por sexo</h3>
+
+		<br>
+
+		Ahora bien, si es a nivel de cantidad de bebés ingresados al programa y haciendo una distinción por el sexo de este, se obtiene la siguiente visualización, donde cada año puede ser visto en detalle al poner el cursor encima del año en la curva:
+
+		<div>
+			<div>
+
+			<iframe src="https://public.tableau.com/views/Flowers_15723185475430/Cantidad?:increment_view_count=no&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&publish=yes&:origin=viz_share_link&:showVizHome=no" width="1004" height="600" frameborder="0"></iframe>
+
+			</div>
+
+		<br>
+		<br>
+
+		La comparación es inmediata, las niñas han visto una fuerte tendencia dentro de los periodos de aumento de RCIU, en que se debe tener un cuidado aún mayor al tener una niña en camino.
+
+		<br>
+		<br>
+
+		</div>
+
+		<h4 align="left">¿Escolaridad con el RCIU?</h4>
+
+		<br>
+
+		Pero ¿qué otro factor puede influir para el aumento de bebés con RCIU? Esto puede ser contestado por varios factores como la nutrición de la madre, el nivel soci-económico, pero en este trabajo se enfocará en el nivel de educación de los padres. Por tal motivo se presenta la siguiente visualización, en que si hay un click dentro del mismo expone más detalle del año, escolaridad y cantidad de RCIU en ese momento: 
+
+		<div>
+			<div>
+
+			<iframe src="https://public.tableau.com/views/Flowers_15723185475430/Cantidad?:increment_view_count=no&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&publish=yes&:origin=viz_share_link&:showVizHome=no" width="1004" height="600" frameborder="0"></iframe>
+
+			</div>
+
+		<br>
+		<br>
+
+		Con base en lo anterior, se reacciona inmediatamente en aquellos de nivel secundaria, donde presentan el mayor índice de RCIU, seguido de la parte técnica y universitaria. Esto puede significar que, sin contar la escolaridad primaria, a un mayor nivel de escolaridad, se reduce el riesgo de que el bebé pueda sufrir de RCIU y puede ser comprobado con visualizaciones de LAURA.
+
+		</div>
+
+		<br>
+		<br>
+
+
 	</div>
 </template>
