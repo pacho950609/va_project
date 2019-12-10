24e895b06239d71320d92ac3f299d4d7e69a4972
Francisco Ricaurte <francisco@tyba.com.co>
2019-12-10 09:01:06 -0500

    Agregar tab

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,7 +25,7 @@
               <a class="nav-link" href="/Tarea3">Impacto Factores familiares</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="/rciu">RCIU</a>
             </li>
           </ul>
         </div>
